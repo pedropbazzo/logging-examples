178d591f5b0867103d9b0ac49ee51f3b22d6af61
shuaicj <shuaicj@gmail.com>
2017-07-26 10:55:14 +0800

    example of jcl

---
+++ logging-jcl/src/main/java/shuaicj/example/logging/jcl/Application.java
@@ -0,0 +1,13 @@
+package shuaicj.example.logging.jcl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Application {
+
+    private static final Log logger = LogFactory.getLog(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello Logging");
+    }
+}
